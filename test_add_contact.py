6b64384fa3d7da8ae372c43ccd24063e79e80f71
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-04-23 13:15:02 +0700

    refactor to pytest test_add_contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,100 +1,29 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from contact import Contact
-import unittest
+from application import Application
+import pytest
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element(by=By.NAME, value="user").clear()
-        wd.find_element(by=By.NAME, value="user").send_keys(username)
-        wd.find_element(by=By.NAME, value="pass").clear()
-        wd.find_element(by=By.NAME, value="pass").send_keys(password)
-        wd.find_element(by=By.XPATH, value="//input[@value='Login']").click()
-
-    def open_contact_page(self, wd):
-        # Open page for add address book entry
-        wd.find_element(by=By.LINK_TEXT, value="add new").click()
-
-    def create_contact(self, wd, contact):
-        self.open_contact_page(wd)
-        # Fill contact form
-        wd.find_element(by=By.NAME, value="firstname").clear()
-        wd.find_element(by=By.NAME, value="firstname").send_keys(contact.firstname)
-        wd.find_element(by=By.NAME, value="middlename").clear()
-        wd.find_element(by=By.NAME, value="middlename").send_keys(contact.middlename)
-        wd.find_element(by=By.NAME, value="lastname").clear()
-        wd.find_element(by=By.NAME, value="lastname").send_keys(contact.lastname)
-        wd.find_element(by=By.NAME, value="nickname").clear()
-        wd.find_element(by=By.NAME, value="nickname").send_keys(contact.nickname)
-        wd.find_element(by=By.NAME, value="title").clear()
-        wd.find_element(by=By.NAME, value="title").send_keys(contact.title)
-        wd.find_element(by=By.NAME, value="company").clear()
-        wd.find_element(by=By.NAME, value="company").send_keys(contact.company)
-        wd.find_element(by=By.NAME, value="address").clear()
-        wd.find_element(by=By.NAME, value="address").send_keys(contact.address)
-        wd.find_element(by=By.NAME, value="home").clear()
-        wd.find_element(by=By.NAME, value="home").send_keys(contact.home_telephone)
-        wd.find_element(by=By.NAME, value="mobile").clear()
-        wd.find_element(by=By.NAME, value="mobile").send_keys(contact.mobile_telephone)
-        wd.find_element(by=By.NAME, value="work").clear()
-        wd.find_element(by=By.NAME, value="work").send_keys(contact.work_telephone)
-        wd.find_element(by=By.NAME, value="fax").clear()
-        wd.find_element(by=By.NAME, value="fax").send_keys(contact.fax)
-        wd.find_element(by=By.NAME, value="email").clear()
-        wd.find_element(by=By.NAME, value="email").send_keys(contact.email)
-        wd.find_element(by=By.NAME, value="email2").clear()
-        wd.find_element(by=By.NAME, value="email2").send_keys(contact.email2)
-        wd.find_element(by=By.NAME, value="email3").clear()
-        wd.find_element(by=By.NAME, value="email3").send_keys(contact.email3)
-        wd.find_element(by=By.NAME, value="homepage").clear()
-        wd.find_element(by=By.NAME, value="homepage").send_keys(contact.homepage)
-        wd.find_element(by=By.NAME, value="address2").clear()
-        wd.find_element(by=By.NAME, value="address2").send_keys(contact.address2)
-        wd.find_element(by=By.NAME, value="phone2").clear()
-        wd.find_element(by=By.NAME, value="phone2").send_keys(contact.phone_home_secondary)
-        wd.find_element(by=By.NAME, value="notes").clear()
-        wd.find_element(by=By.NAME, value="notes").send_keys(contact.notes)
-        # Submit contact creation
-        wd.find_element(by=By.NAME, value="submit").click()
-        self.return_to_contact_page(wd)
-
-    def return_to_contact_page(self, wd):
-        wd.find_element(by=By.LINK_TEXT, value="home").click()
-
-    def logout(self, wd):
-        wd.find_element(by=By.LINK_TEXT, value="Logout").click()
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Contact(firstname="234", middlename="123", lastname="qwer", nickname="asdf",
-                            title="zxczv", company="erty", address="dfgh", home_telephone="xcvb",
-                            mobile_telephone="tyui", work_telephone="ghj", fax="mnbv", email="[iop", email2="jhkl",
-                            email3="uiop", homepage="bnm,", address_secondary="5t5t5", phone_home_secondary="tgtgt",
-                            notes="vfvfv"))
-        self.logout(wd)
-
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-                            address="", home_telephone="", mobile_telephone="", work_telephone="", fax="", email="",
-                            email2="", email3="", homepage="", address_secondary="", phone_home_secondary="", notes=""))
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="234", middlename="123", lastname="qwer", nickname="asdf",
+                       title="zxczv", company="erty", address="dfgh", home_telephone="xcvb",
+                       mobile_telephone="tyui", work_telephone="ghj", fax="mnbv", email="[iop", email2="jhkl",
+                       email3="uiop", homepage="bnm,", address_secondary="5t5t5", phone_home_secondary="tgtgt",
+                       notes="vfvfv"))
+    app.logout()
+
+
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
+                       address="", home_telephone="", mobile_telephone="", work_telephone="", fax="", email="",
+                       email2="", email3="", homepage="", address_secondary="", phone_home_secondary="", notes=""))
+    app.logout()
