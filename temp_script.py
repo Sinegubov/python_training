5e0d6ac3048163303079c52d51d9f9c854ea4747
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-04-25 10:26:55 +0700

    create packege for different things

--- temp_script.py
+++ temp_script.py
@@ -5,3 +5,18 @@ print([lens-2])
 print((s[:5]))
 print(s[:lens-2])
 print(lens)
+
+
+
+####Списки
+
+list('Python')
+s = []
+l = ['s','p',['isok'], 2]
+print (s)
+print (l)
+
+var = ['l','i','s','t']
+#var.extend(['l','i','s','t'])
+var.sort()
+print(var)
