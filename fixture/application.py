eefb4c966917552c6cb4810ebd3f9681f5c3aafe
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-04-25 10:49:26 +0700

    Allocated utility class for work with group for test_add_group.py

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -9,33 +10,11 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element(by=By.LINK_TEXT, value="groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # Init group creation
-        wd.find_element(by=By.NAME, value="new").click()
-        # Fill group form
-        wd.find_element(by=By.NAME, value="group_name").click()
-        wd.find_element(by=By.NAME, value="group_name").clear()
-        wd.find_element(by=By.NAME, value="group_name").send_keys(group.name)
-        wd.find_element(by=By.NAME, value="group_header").click()
-        wd.find_element(by=By.NAME, value="group_header").clear()
-        wd.find_element(by=By.NAME, value="group_header").send_keys(group.header)
-        wd.find_element(by=By.NAME, value="group_footer").click()
-        wd.find_element(by=By.NAME, value="group_footer").clear()
-        wd.find_element(by=By.NAME, value="group_footer").send_keys(group.footer)
-        # submit groups creation
-        wd.find_element(by=By.NAME, value="submit").click()
-        self.return_to_groups_page()
 
     def open_contact_page(self):
         wd = self.wd
@@ -89,10 +68,6 @@ class Application:
     def return_to_contact_page(self):
         wd = self.wd
         wd.find_element(by=By.LINK_TEXT, value="home").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element(by=By.LINK_TEXT, value="group page").click()
 
     def destroy(self):
         self.wd.quit()
