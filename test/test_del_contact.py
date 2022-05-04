27d4f747a0e56ceb43711ea3799a99b2ce10f6ca
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-05-04 11:19:33 +0700

    fix condition for contact tests

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,5 +4,5 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.modify_first_contact(Contact(firstname="Some test Name"))
+        app.contact.create(Contact(firstname="Some test Name"))
     app.contact.delete_first_contact()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,11 +4,11 @@ from model.contact import Contact
 
 def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
-        app.contact.modify_first_contact(Contact(firstname="Some test Name"))
+        app.contact.create(Contact(firstname="Some test Name"))
     app.contact.modify_first_contact(Contact(firstname="New FirstName"))
 
 
 def test_modify_contact_company(app):
     if app.contact.count() == 0:
-        app.contact.modify_first_contact(Contact(company="Some test Name"))
+        app.contact.create(Contact(company="Some test Name"))
     app.contact.modify_first_contact(Contact(company="New CompanyName"))
