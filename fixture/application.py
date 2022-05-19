b383b919fd5ffeb152bb34e33d982fddb2970f08
kampik <sineguboff@gmail.com>
2022-05-20 00:32:01 +0700

    add cmd options

--- fixture/application.py
+++ fixture/application.py
@@ -7,13 +7,21 @@ from fixture.contact_merge_function import ContactInfoHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.contact_merge_function = ContactInfoHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +32,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
