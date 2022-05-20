824b9caea3f7f19823bcf55b1902ad487e578cfd
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-05-20 19:30:19 +0700

    refactor conftest for work with testdata

--- generator/group.py
+++ generator/group.py
@@ -32,6 +32,13 @@ testdata_n = [Group(name="", footer="", header="")] + [
     for i in range(n)
 ]
 
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
+]
+
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
