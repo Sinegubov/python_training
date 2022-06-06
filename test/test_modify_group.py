b789b9e557afc645dd7027d2058af07022f64bb0
kampik <sineguboff@gmail.com>
2022-06-06 11:22:42 +0700

    return ui checks for modify tests for group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 import random
 
 
-def test_modify_group_name(app, db, check_ui):
+def test_modify_group_name_db(app, db, check_ui):
     group = Group(name="New group")
     if len(db.get_group_list()) == 0:
         app.group.create(group)
@@ -22,6 +23,20 @@ def test_modify_group_name(app, db, check_ui):
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == \
                sorted(app.group.get_contact_list(), key=Group.id_or_max)
+
+
+def test_modify_group_name_ui(app):
+    group = Group(name="New group")
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
+    assert len(old_groups) == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups[index] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app, db, check_ui):
