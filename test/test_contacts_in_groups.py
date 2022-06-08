42e525b7aac243d51cd0cd3eb66332eeadb22e38
kampik <sineguboff@gmail.com>
2022-06-09 00:25:28 +0700

    fix tests contacts in group| task22

--- test/test_contacts_in_groups.py
+++ test/test_contacts_in_groups.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 import random
+import data.contact
+import data.groups
 from model.contact import Contact
-from model.group import Group
 
 
 def test_add_contact_to_group(app, orm):
     contact = None
-    # add_to_group = None
     all_groups = orm.get_group_list()
     if len(all_groups) == 0:
-        app.group.create(Group(name="SomeName", header="SomeHeader", footer="SomeFooter"))
+        app.group.create(data.groups.testdata[0])
         all_groups = orm.get_group_list()
-        group = all_groups[len(all_groups)-1]
+        # group = all_groups[len(all_groups)-1]
     if contact is None:
-        app.contact.create(Contact(firstname="name1", middlename="middl", lastname="lasst", nickname="nick", title="T",
-                                   company="Apple", address="Earth", home_telephone="12332123",
-                                   mobile_telephone="3454345", work_telephone="5676567", fax="44444444",
-                                   email="asd@asd.ASd", email2="qwe_ewq@w.w.w.e", email3="email",
-                                   homepage="http://asd.asd", address_secondary="MilkyWay",
-                                   phone_home_secondary="876867876", notes="zzzz"))
+        app.contact.create(data.contact.testdata[0])
         contacts = sorted(orm.get_contact_list(), key=Contact.id_or_max)
         contact = contacts[len(contacts)-1]
     for group in all_groups:
@@ -35,10 +30,9 @@ def test_add_contact_to_group(app, orm):
 
 def test_del_contact_from_group(app, orm):
     contact = None
-    # add_to_group = None
     all_groups = orm.get_group_list()
     if len(all_groups) == 0:
-        app.group.create(Group(name="SomeName", header="SomeHeader", footer="SomeFooter"))
+        app.group.create(data.groups.testdata[1])
         app.contact.add_contact_to_group(random.choice(orm.get_contact_list()), random.choice(orm.get_group_list()))
         all_groups = orm.get_group_list()
     for group in all_groups:
@@ -48,12 +42,7 @@ def test_del_contact_from_group(app, orm):
             add_to_group = group
             break
     if contact is None and orm.get_contact_list() == 0:
-        app.contact.create(Contact(firstname="name2", middlename="midd2", lastname="lasst", nickname="nick", title="T",
-                           company="Apple", address="Earth", home_telephone="12332123",
-                           mobile_telephone="3454345", work_telephone="5676567", fax="44444444",
-                           email="asd@asd.ASd", email2="qwe_ewq@w.w.w.e", email3="email",
-                           homepage="http://asd.asd", address_secondary="MilkyWay",
-                           phone_home_secondary="876867876", notes="zzzz"))
+        app.contact.create(data.contact.testdata[1])
         contact = orm.get_contact_list()[0]
         add_to_group = random.choice(orm.get_group_list())
         app.contact.add_contact_to_group(contact, add_to_group)
