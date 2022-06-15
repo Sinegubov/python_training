c8dd09fbb57bfb0e1d309d0eba9e2b18e6958548
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-06-15 14:52:12 +0700

    WIP task22

--- test/test_contacts_in_groups.py
+++ test/test_contacts_in_groups.py
@@ -5,33 +5,23 @@ import data.groups
 
 
 def test_add_contact_to_group(app, db, orm):
-    all_groups = orm.get_group_list()
-    all_contacts = orm.get_contact_list()
-    if not all_groups:
+    if not orm.get_group_list():
         app.group.create(data.groups.testdata[0])
-        all_groups = orm.get_group_list()
-    if not all_contacts:
+    if not orm.get_contact_list():
         app.contact.create(data.contact.testdata[0])
-        all_contacts = orm.get_contact_list()
-    group = random.choice(all_groups)
-    contact = random.choice(all_contacts)
-    db_contacts_in_group = orm.get_contacts_in_group(group)
+    group = random.choice(orm.get_group_list())
     db_contacts_not_in_group = orm.get_contacts_not_in_group(group)
-    if contact not in db_contacts_in_group:
+    old_list_contacts_in_group = orm.get_contacts_in_group(group)
+    if not db_contacts_not_in_group:
+        app.contact.create(data.contact.testdata[1])
+        contact = db.get_contact_list()[-1]
         app.contact.add_contact_to_group(contact, group)
     else:
-        if db_contacts_not_in_group:
-            contact = random.choice(db_contacts_not_in_group)
-            app.contact.add_contact_to_group(contact, group)
-        else:
-            if not db_contacts_not_in_group:
-                app.contact.create(data.contact.testdata[0])
-                contact = db.get_contact_list()[-1]
-                app.contact.add_contact_to_group(contact, group)
-    app.contact.add_contact_to_group(contact, group)
-    new_list_contacts = orm.get_contacts_in_group(group)
-    assert contact in new_list_contacts
-    assert len(db_contacts_in_group) + 1 == len(new_list_contacts)
+        contact = random.choice(db_contacts_not_in_group)
+        app.contact.add_contact_to_group(contact, group)
+    new_list_contacts_in_group = orm.get_contacts_in_group(group)
+    assert contact in orm.get_contacts_in_group(group)
+    assert len(old_list_contacts_in_group) + 1 == len(new_list_contacts_in_group)
 
 
 def test_del_contact_from_group(app, db, orm):
